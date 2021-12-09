55d8d406e66d829d6e38f922d8b901b0fa29f456
sztodo <57600228+sztodo@users.noreply.github.com>
2021-12-09 14:36:14 +0200

    lab10.16

--- Mihaela_Sztodolnik_Lab10/App.xaml.cs
+++ Mihaela_Sztodolnik_Lab10/App.xaml.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Mihaela_Sztodolnik_Lab10.Data;
+using System.IO;
 
 namespace Mihaela_Sztodolnik_Lab10
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new ListEntryPage());
         }
 
         protected override void OnStart()
---
+++ Mihaela_Sztodolnik_Lab10/ListEntryPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mihaela_Sztodolnik_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListEntryPage : ContentPage
+    {
+        public ListEntryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
