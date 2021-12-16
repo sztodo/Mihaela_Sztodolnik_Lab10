96971312f5d2b60d325fad494a1dfdc91f936136
sztodo <57600228+sztodo@users.noreply.github.com>
2021-12-16 14:54:44 +0200

    lab11.12

--- Mihaela_Sztodolnik_Lab10/Data/ShoppingListDatabase.cs
+++ Mihaela_Sztodolnik_Lab10/Data/ShoppingListDatabase.cs
@@ -14,6 +14,28 @@ namespace Mihaela_Sztodolnik_Lab10.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
+        }
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
         }
         public Task<List<ShopList>> GetShopListsAsync()
         {
--- Mihaela_Sztodolnik_Lab10/ListPage.xaml.cs
+++ Mihaela_Sztodolnik_Lab10/ListPage.xaml.cs
@@ -30,5 +30,13 @@ namespace Mihaela_Sztodolnik_Lab10
             await App.Database.DeleteShopListAsync(slist);
             await Navigation.PopAsync();
         }
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
     }
 }
